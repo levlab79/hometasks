f46850e5be860962c2155b03a496f2e4867712a8
levlab79 <lev.lsb56@gmail.com>
2020-04-15 15:35:54 +0200

    checked all my tasks in the first homework

--- Berezianskii_Lev_dz_1/task_1.py
+++ Berezianskii_Lev_dz_1/task_1.py
@@ -13,9 +13,12 @@ print(a_str)
 a_bool = False
 print(a_bool)
 # Запросим у пользователя несколько число и строк, и сохраним их в переменные.
-b_int = int(input('Введите целое число: '))
-b_float = float(input('Введите число с плавающей точкой: '))
-b_str = input('Введите строку: ')
+# b_int = int(input('Введите целое число: '))
+b_int = int('21')
+# b_float = float(input('Введите число с плавающей точкой: '))
+b_float = float('4.02')
+# b_str = input('Введите строку: ')
+b_str = 'some string'
 # Выведем их на экран.
 print(b_int)
 print(b_float)
