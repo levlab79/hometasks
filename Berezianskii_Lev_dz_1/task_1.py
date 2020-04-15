994bffa68a028fbe745c389abfc5abc4430e5b0c
levlab79 <lev.lsb56@gmail.com>
2020-04-15 15:22:41 +0200

    did all my homework

--- Berezianskii_Lev_dz_1/task_1.py
+++ Berezianskii_Lev_dz_1/task_1.py
@@ -1,1 +1,22 @@
-# to do
+# Задание 1.
+# Создадим 4 переменные разных типов и выведем их на экран.
+# Переменная типа int.
+a_int = 21
+print(a_int)
+# Переменная типа float.
+a_float = 3.21
+print(a_float)
+# Переменная типа str.
+a_str = 'Strange string.'
+print(a_str)
+# Переменная типа bool.
+a_bool = False
+print(a_bool)
+# Запросим у пользователя несколько число и строк, и сохраним их в переменные.
+b_int = int(input('Введите целое число: '))
+b_float = float(input('Введите число с плавающей точкой: '))
+b_str = input('Введите строку: ')
+# Выведем их на экран.
+print(b_int)
+print(b_float)
+print(b_str)
